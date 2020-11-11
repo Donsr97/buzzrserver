7fad7085d3e3a7ae71290a537fc513b8e1697435
Donaldo Salazar <donaldosalazar1@gmail.com>
2020-11-10 22:49:59 -0600

    Update posts.routes.js

--- app/routes/posts.routes.js
+++ app/routes/posts.routes.js
@@ -1,19 +1,12 @@
 module.exports = app => {
   const post = require("../controllers/posts.controller.js");
-
   var router = require("express").Router();
-
   router.post("/", post.create);
-
   router.get("/", post.findAll);
-
+  router.get("/usr/:iduser", post.findUsers);
   router.get("/:id", post.findOne);
-
   router.put("/:id", post.update);
-
   router.delete("/:id", post.delete);
-
   router.delete("/", post.deleteAll);
-
   app.use('/api/post', router);
 };
